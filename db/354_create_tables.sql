acb409ac94f75cbfa5881a47555b7c73415cb3b8
PhilCAEUser <rbyETE4Ceu2iA4Z>
2020-07-06 07:59:50 +0000

    Test version number

---
+++ db/354_create_tables.sql
@@ -0,0 +1,16 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service 354, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table tab2.
+--
+CREATE TABLE commedit.tab2 (
+  col2 INTEGER ,
+CONSTRAINT col2_PK PRIMARY KEY (col2)
+ 
+);
+
+
+
